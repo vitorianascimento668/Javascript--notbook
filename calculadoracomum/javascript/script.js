2581718121eecea2bf6908382607a59d0b9e7b11
Vitoria Nascimento <vitorianascimentoesilva9@gmail.com>
2022-12-10 22:32:13 -0300

    Terminei o HTML/CSS e comecei o javascript
    Agora comecei o javascript, apos termina o HTML, nele fiz um flex nas minhas duas div e depois arrumei a minha class .resposta, agora eu vou comear a mexe o javascript, sendo que ja adicionei o data-oper para pega a operacao que o meu usario clicou e comear a fazer as contas, também adicionei duas varaveis para pega os numeros, porém acredito que irei precisa de mais

--- calculadoracomum/javascript/script.js
+++ calculadoracomum/javascript/script.js
@@ -1,14 +1,35 @@
 let visor = document.querySelector('.visor')
 let num = document.querySelectorAll('.num')
+let operac = document.querySelectorAll('.operac')
 let dataNum = 0
+let operac_valor = ''
+let v1 = []
+let v2 = []
+let auxiliadora = 0
 
 function numeros(){
-    
+
        num.forEach((e)=>{
         e.addEventListener('click',()=>{
             dataNum = e.getAttribute('data-num')
-           visor.innerHTML += dataNum
+            if(auxiliadora == 0 ){
+                v1.push( dataNum)
+                visor.innerHTML += dataNum   
+
+        }
+            else{
+                v2.push(dataNum)
+                visor.innerHTML += dataNum   
+
+        }
        })
+        })
+        operac.forEach((evento) =>{
+            evento.addEventListener("click",()=>{
+                operac_valor = evento.getAttribute('data-ope')
+                visor.innerHTML += operac_valor
+                auxiliadora ++
+            })
         })
  
     }
