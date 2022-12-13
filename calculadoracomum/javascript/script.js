ae2a7771a08d6d8befc692a7e67377d36f07bdd3
Vitoria Nascimento <vitorianascimentoesilva9@gmail.com>
2022-12-12 21:05:44 -0300

    Consegui coloca os valores para adiona na varival V1 para enrão começa a mostra na tela.
    Comecei a mostra no meu console quais são os valores para a variavel V1, pos ela sera a repsonsavel por pega os valores dentro da calculadora, e futuramente preencher os valores e assim mostra na tela que ira mostra a seguir, .resultado, agora eu precjso fazer mais algumas prevenções de erros, e depois vou comea a mostra os resultados na tela.

--- calculadoracomum/javascript/script.js
+++ calculadoracomum/javascript/script.js
@@ -4,34 +4,66 @@ let operac = document.querySelectorAll('.operac')
 let dataNum = 0
 let operac_valor = ''
 let v1 = []
-let v2 = []
-let auxiliadora = 0
+let au = 0
+var v2 = ''
+
 
 function numeros(){
+    operac.forEach((evento) =>{
+        evento.addEventListener("click",()=>{
+            operac_valor = evento.getAttribute('data-ope')
 
+            if(operac_valor != '=')    {
+                visor.innerHTML += ` ${operac_valor} `
+
+            }
+           
+            if(operac_valor.length > 0){
+                if (v2.length > 0){               
+                v1.push(v2)
+                v2 = ''
+                operac_valor = ''
+            }else{
+                alert('ERRO, SELECIONE UM VALOR')
+            }
+        } 
+
+        })
+    })
        num.forEach((e)=>{
         e.addEventListener('click',()=>{
             dataNum = e.getAttribute('data-num')
-            if(auxiliadora == 0 ){
-                v1.push( dataNum)
+    
+                v2 += dataNum
                 visor.innerHTML += dataNum   
 
-        }
-            else{
-                v2.push(dataNum)
-                visor.innerHTML += dataNum   
+       })          
 
-        }
-       })
+            
         })
-        operac.forEach((evento) =>{
-            evento.addEventListener("click",()=>{
-                operac_valor = evento.getAttribute('data-ope')
-                visor.innerHTML += operac_valor
-                auxiliadora ++
-            })
-        })
+
  
     }
+function igual(){
+        v1.forEach((num)=>{
+            if (operac_valor === '+'){
+                au += Number(num)
+
+                
+            }else if(operac_valor === '-'){
+                au -= Number(num)
+            }
+
+        })
+
+
+}
 
 numeros()
+
+/*
+1-) corrigir o valor para cair inteiro dentro da variavel, sem quebra, antes de aperta o valor de operação
+2-) preencher a tela que aparece com a conta inteira aberta
+3-)fazer a soma, subtração e adicão funcionar
+
+*/
