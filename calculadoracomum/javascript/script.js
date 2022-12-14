004105715f26089c1999e82d8d4eb37db22085fc
Vitoria Nascimento <vitorianascimentoesilva9@gmail.com>
2022-12-13 22:45:54 -0300

    Começei a fazer a tela de resultado aparece
    Comecei a fazer a tela de resultado aparece, além de ter criado uma função para altera a tela, para fica tudo alinhado quando essa tela for aparece, isso é quando clicarmos em = , agora precis fazer uma filtragem para ver se o valor já foi exposto na tela, se foi não vai mais repetir, senão ira continua repetindo, apoos isso começarei as contas em si

--- calculadoracomum/javascript/script.js
+++ calculadoracomum/javascript/script.js
@@ -1,31 +1,54 @@
-let visor = document.querySelector('.visor')
+let visor = document.querySelector(".visor")
 let num = document.querySelectorAll('.num')
 let operac = document.querySelectorAll('.operac')
+let resultado_1= document.querySelector('.valor1')
+let resultado_amais = document.querySelector('.valor2')
+let resultado = document.querySelector('.resultado')
+let digitos = document.querySelector('.digitos')
 let dataNum = 0
 let operac_valor = ''
+let tirando_repetição = []
 let v1 = []
 let au = 0
 var v2 = ''
-
 
 function numeros(){
     operac.forEach((evento) =>{
         evento.addEventListener("click",()=>{
             operac_valor = evento.getAttribute('data-ope')
-
-            if(operac_valor != '=')    {
+            if(operac_valor == '='){
+                /* aqui ainda está dando alguma demora de
+                resposta, sendo necessario teclar 
+                2x no = para funciona o limpa 
+                tela e variavel*/
+                limpaTela()
+            }
+            if(operac_valor != '=' )    {
                 visor.innerHTML += ` ${operac_valor} `
 
             }
-           
+
+
+            /* Aqui se o usuario digita um valor após aperta o 
+            igual, a tela, tem que ser limpa e todas as variaveis
+             apos ser limpa e a tela também*/
+             
             if(operac_valor.length > 0){
                 if (v2.length > 0){               
                 v1.push(v2)
                 v2 = ''
-                operac_valor = ''
-            }else{
-                alert('ERRO, SELECIONE UM VALOR')
             }
+            for (let i in v1){
+
+                
+                if(i == 0 ){
+                    resultado_1.innerHTML = v1[i]
+                }else if(tirando_repetição.indexOf(el) == -1){
+                    resultado_amais.innerHTML += `${v1[i]} </br>`
+                }
+                console.log(v1.length)
+    }
+            
         } 
 
         })
@@ -44,18 +67,27 @@ function numeros(){
 
  
     }
+
+function limpaTela(){
+    if(v2.length < 1){
+        v1 = []
+        visor.innerHTML = ''
+    }
+}
+    
 function igual(){
-        v1.forEach((num)=>{
-            if (operac_valor === '+'){
-                au += Number(num)
+    
+    abrirMeiaTela()
+}
 
-                
-            }else if(operac_valor === '-'){
-                au -= Number(num)
-            }
 
-        })
 
+
+function  abrirMeiaTela(){
+    digitos.classList.add('abriu')
+    visor.classList.add('abriu_visor')
+    resultado.style.display = 'flex'
+    resultado.style.left = '0px'
 
 }
 
