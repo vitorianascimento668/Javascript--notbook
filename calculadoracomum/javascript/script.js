290c5347078189792133d0daf20a41159bd55480
Vitoria Nascimento <vitorianascimentoesilva9@gmail.com>
2022-12-17 18:21:42 -0300

    Conserto do erro do igual, agora o resultado aparece tudo ao mesmo tempo
    Consegui conserta o erro do igual, agora aparece o resultado exatamente quando o igual for apertado ou seja a funcao igual() for solicitada.

--- calculadoracomum/javascript/script.js
+++ calculadoracomum/javascript/script.js
@@ -7,36 +7,46 @@ let resultado = document.querySelector('.resultado')
 let digitos = document.querySelector('.digitos')
 let sinais_ope = document.querySelector('.sinaldaOpe')
 let tela_res = document.querySelector('.res')
-let iguale = document.querySelector('.igual')
+let multiplica = 0
 let dataNum = 0
+let calculo = 0
 let au = ''
 var v2 = ''
 let operac_valor = ''
-let calculo = 0
 let v1 = []
 
 
 function numeros(){
+     num.forEach((e)=>{
+        e.addEventListener('click',()=>{
+            dataNum = e.getAttribute('data-num')
+    
+                v2 += dataNum
+                visor.innerHTML += dataNum                      
+       })         
+
+        })
+
+       
+    }
+function opera (){
     operac.forEach((evento) =>{
         evento.addEventListener("click",()=>{
             operac_valor = evento.getAttribute('data-ope')
             limpaResultado()
-            if(operac_valor != '='){
-                au = operac_valor
-            }
 
-            if(operac_valor == '='){
-                
-                igual()
+
+
+
                 /* aqui ainda está dando alguma demora de
                 resposta, sendo necessario teclar 
                 2x no = para funciona o limpa 
                 tela e variavel*/
-                limpaTela()
-            }
+  
+
             if(operac_valor != '=' )    {
                 visor.innerHTML += ` ${operac_valor} `
-
+                au = operac_valor
             }
  
 
@@ -57,24 +67,30 @@ function numeros(){
                  if(operac_valor == "+" || au == "+"){
                     calculo+=Number(v2)
                 }
+               /* if(operac_valor == "*"|| au == "*"){
+                    if (calculo == 0 ){
+                        multiplica = Number(v2)
+                    }else{
+                        
+                        calculo = multiplica * Number(v2)
+                        multiplica = Number(v2)
+                    }
+                }*/
                     
-                v1.push(v2)
+                v1.push(v2) 
                 
 
                     v2 = '' 
             }
-
-
             for (let i in v1){
-                if(operac_valor != '='){
-                sinais_ope.innerHTML = operac_valor}
+            if(operac_valor != '='){
+            sinais_ope.innerHTML =`${operac_valor} </br>` }
 
-                if(i == 0 ){
-                    resultado_1.innerHTML = v1[i]
-                }else {
-                    resultado_amais.innerHTML += `${v1[i]} </br>`
-                }
-                
+            if(i == 0 ){
+                resultado_1.innerHTML = v1[i]
+            }else {
+                resultado_amais.innerHTML += `${v1[i]} </br>`
+            }                
 
     }
 
@@ -83,23 +99,24 @@ function numeros(){
         } 
         })
     })
-       num.forEach((e)=>{
-        e.addEventListener('click',()=>{
-            dataNum = e.getAttribute('data-num')
-    
-                v2 += dataNum
-                visor.innerHTML += dataNum   
+      
+}
+        
 
-       })         
-
-        })
-
-       
-    }
     let i = 0
 function igual(){
-    
+    if(operac_valor == "-" || au == "-"){
+        if(calculo == 0){
+            calculo = v2
+        }else{
+            calculo-=Number(v2)}
+    }
+     if(operac_valor == "+" || au == "+"){
+        calculo+=Number(v2)
+    }
+    tela_res.innerHTML = calculo
     abrirMeiaTela()
+    limpaResultado()
 
 }
 
@@ -125,6 +142,7 @@ function  abrirMeiaTela(){
 
 }
 numeros()
+opera()
 
 /*
 1-) corrigir o valor para cair inteiro dentro da variavel, sem quebra, antes de aperta o valor de operação
