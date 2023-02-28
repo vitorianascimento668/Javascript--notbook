357c5044cc0ef4532a3c3ad38fced8b9cd3e2ac6
Vitoria Nascimento <vitorianascimentoesilva9@gmail.com>
2023-02-27 22:18:22 -0300

    Tirei a repetição das bombas
    Comecei a adicionar funcionalidades no meu jogo, sendo que somente o sinal de "ok" aparecera, quando eu aperta a variavel vazia, caso contrario, e porque a bomba foi adicionada no quadrinho

--- campoMinado/javascipt/scrpt.js
+++ campoMinado/javascipt/scrpt.js
@@ -1,11 +1,20 @@
 let quadrinhos = document.querySelectorAll(".quadro")
-let sorteio, c
-for (c=0; c < 5; c++){
-    sorteio =  Math.floor(Math.random() * (0 - 30 +1)) + 30
-    quadrinhos[sorteio].innerHTML += "💣"
+let sorteio, c = 0
+
+while(c < 5){
+    sorteio =  Math.floor(Math.random() * (0 - 29 +1)) + 29
+    if(quadrinhos[sorteio].innerHTML != "💣"){
+        quadrinhos[sorteio].innerHTML += "💣"
+        c++
+    }
 }
 quadrinhos.forEach((quadros)=>{
     quadros.addEventListener("click",()=>{
-      
+      if (quadros.innerHTML != "💣"){
+        quadros.innerHTML += "✔️" 
+      }else 
+      if (quadros.innerHTML == "💣"){
+        alert("NAOUM")
+      }
     })
 })
