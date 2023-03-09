7fbe277347087e6c29f570f0f7d262744fc754ee
Vitoria Nascimento <vitorianascimentoesilva9@gmail.com>
2023-03-08 23:11:19 -0300

    Funcao de cor esta funcional e tudo assimetrico
    criei a variavel texto caixa pea termina de passa todas as cores corretamente, depois consrtei o meu switch case, pos eu coloquei dois igual e na verdade era apenas um , pra fazer a comparação.
    De restante, fiz uma coisa simples, apenas pega o valor da cor e coloca div necssaria
    
    tambem coloquei uma borda botter na minha marginde cima, pra fica mais assimetrica.

--- listadetarefas/js/script.js
+++ listadetarefas/js/script.js
@@ -4,6 +4,7 @@ let cores_display = document.querySelector(".cores_fundo")
 let body_meio = document.querySelector(".body-meio")
 let margin_cima = document.querySelector(".margin-cima")
 let footer_baixo = document.querySelector(".footer-baixo")
+let texto_caixa = document.querySelector(".texto_caixa")
 let  valor_cor
 
 
@@ -18,44 +19,70 @@ function tresPontos(){
     cores.forEach((cores)=>{
         cores.addEventListener("click",()=>{
             valor_cor = cores.getAttribute("data-cores")
-            mudança_cor()
+            mudanca_cor()
             cores_display.style.display = "none"
         })
     })
     
 }
-function  mudança_cor(){
+function  mudanca_cor(){
+    
     switch (valor_cor){
-        case valor_cor == "amarelo":
-           
+        
+        case valor_cor = "amarelo":
+           body_meio.style.backgroundColor="rgb(250, 250, 32)"
+           margin_cima.style.backgroundColor="rgb(250, 250, 32)"
+           footer_baixo.style.backgroundColor="rgb(250, 250, 32)"
+           texto_caixa.style.backgroundColor="rgb(250, 250, 32)"
             
         break
-        case valor_cor == "verde":
-           
+        case valor_cor = "verde":
+            body_meio.style.backgroundColor="rgb(141, 199, 141)"
+            margin_cima.style.backgroundColor="rgb(141, 199, 141)"
+            footer_baixo.style.backgroundColor="rgb(141, 199, 141)"
+            texto_caixa.style.backgroundColor="rgb(141, 199, 141)"
             
         break
-        case valor_cor == "lilais":
-           
+        case valor_cor = "lilais":
+            body_meio.style.backgroundColor="rgb(255, 154, 250)"
+            margin_cima.style.backgroundColor="rgb(255, 154, 250)"
+            footer_baixo.style.backgroundColor="rgb(255, 154, 250)"
+            texto_caixa.style.backgroundColor="rgb(255, 154, 250)"
             
         break
-        case valor_cor == "rosa":
-           
+        case valor_cor = "rosa":
+            body_meio.style.backgroundColor="rgb(230, 42, 145)"
+            margin_cima.style.backgroundColor="rgb(230, 42, 145)"
+            footer_baixo.style.backgroundColor="rgb(230, 42, 145)"
+            texto_caixa.style.backgroundColor="rgb(230, 42, 145)"
             
         break
-        case valor_cor == "red":
-           
+        case valor_cor = "red":
+            body_meio.style.backgroundColor="red"
+            margin_cima.style.backgroundColor="red"
+            footer_baixo.style.backgroundColor="red"
+            texto_caixa.style.backgroundColor="red"
             
         break
-        case valor_cor == "azul":
-           
+        case valor_cor = "azul":
+            body_meio.style.backgroundColor="rgb(147, 192, 218)"
+            margin_cima.style.backgroundColor="rgb(147, 192, 218)"
+            footer_baixo.style.backgroundColor="rgb(147, 192, 218)"
+            texto_caixa.style.backgroundColor="rgb(147, 192, 218)"
             
         break
-        case valor_cor == "cinza":
-           
+        case valor_cor = "cinza":
+            body_meio.style.backgroundColor="rgb(163, 154, 154)"
+            margin_cima.style.backgroundColor="rgb(163, 154, 154)"
+            footer_baixo.style.backgroundColor="rgb(163, 154, 154)"
+            texto_caixa.style.backgroundColor="rgb(163, 154, 154)"
             
         break
-        case valor_cor == "preto":
-           
+        case valor_cor = "preto":
+            body_meio.style.backgroundColor="rgb(66 66 66)"
+            margin_cima.style.backgroundColor="rgb(66 66 66)"
+            footer_baixo.style.backgroundColor="rgb(66 66 66)"
+            texto_caixa.style.backgroundColor="rgb(66 66 66)"
             
         break
 
