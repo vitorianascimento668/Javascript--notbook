f1749f70e585f5f4002680aa8ed4e4d1dfba5a06
Vitoria Nascimento <vitorianascimentoesilva9@gmail.com>
2023-03-08 22:30:46 -0300

    funçao mudança_cor() e princiapis valors pego
    resolvi pega as variaveis do body do meio, a de cima e a de baixo no caso o footer e depois coloque na minha funcao tresPontos pra chama a mudançacor() e na minha funcao mudança eu estou usando um switch que recebe o valor de valor_cor e vai fazer a comparação com cada cor, e assim vamos começa a estilizar

--- listadetarefas/js/script.js
+++ listadetarefas/js/script.js
@@ -1,7 +1,9 @@
 let mais_bot = document.querySelector(".botao_mais")
 let cores = document.querySelectorAll(".cores")
 let cores_display = document.querySelector(".cores_fundo")
-
+let body_meio = document.querySelector(".body-meio")
+let margin_cima = document.querySelector(".margin-cima")
+let footer_baixo = document.querySelector(".footer-baixo")
 let  valor_cor
 
 
@@ -16,12 +18,49 @@ function tresPontos(){
     cores.forEach((cores)=>{
         cores.addEventListener("click",()=>{
             valor_cor = cores.getAttribute("data-cores")
+            mudança_cor()
             cores_display.style.display = "none"
         })
     })
     
 }
+function  mudança_cor(){
+    switch (valor_cor){
+        case valor_cor == "amarelo":
+           
+            
+        break
+        case valor_cor == "verde":
+           
+            
+        break
+        case valor_cor == "lilais":
+           
+            
+        break
+        case valor_cor == "rosa":
+           
+            
+        break
+        case valor_cor == "red":
+           
+            
+        break
+        case valor_cor == "azul":
+           
+            
+        break
+        case valor_cor == "cinza":
+           
+            
+        break
+        case valor_cor == "preto":
+           
+            
+        break
 
+    }
+}
 
 
 
