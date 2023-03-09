03ec2652e48559d4a9041ac784ff5f38835c26fd
Vitoria Nascimento <vitorianascimentoesilva9@gmail.com>
2023-03-08 22:10:29 -0300

    Criei a funcao tresPontos e adicionei outro onclick dentro dela
    criei duas variaveis para pega as cores com o selectorAll e o valor da cor, pra pega o atributo dela, quando houver um click
    
    Também deixei o cores_display como flex quando houver click no tresPontos(), e apos fazer um for each e adiciona o click nas cores que irao aparece na tela pega o atributo, e depois os cores_display fica none novamente. (Tecnica diferente da minha antiga) aonde eu usa o if, agora não precisa usa estrutura de condição!

--- listadetarefas/js/script.js
+++ listadetarefas/js/script.js
@@ -1,13 +1,25 @@
 let mais_bot = document.querySelector(".botao_mais")
-let cores = document.querySelector(".cores")
+let cores = document.querySelectorAll(".cores")
 let cores_display = document.querySelector(".cores_fundo")
+
+let  valor_cor
 
 
 
 
 
 function tresPontos(){
-    cores_display.style.display = "flex"
+    
+        cores_display.style.display = "flex"
+    
+
+    cores.forEach((cores)=>{
+        cores.addEventListener("click",()=>{
+            valor_cor = cores.getAttribute("data-cores")
+            cores_display.style.display = "none"
+        })
+    })
+    
 }
 
 
